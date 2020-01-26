32f98962c03082aabf7a6600e19860034838015e
Victoria Orlova <victoria1orlova@gmail.com>
2020-01-26 17:23:08 +0200

    First App 'Hello Word'

---
+++ cb/starter/L1/HelloWordApp/HelloWordApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWordApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
