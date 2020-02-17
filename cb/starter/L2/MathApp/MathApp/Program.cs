43c7bf31b54c30758ffe95c91a83665bd10717a2
Victoria Orlova <victoria1orlova@gmail.com>
2020-02-17 18:43:22 +0200

    MathApp is done

--- cb/starter/L2/MathApp/MathApp/Program.cs
+++ cb/starter/L2/MathApp/MathApp/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace MathApp
+﻿using System;
+
+namespace MathApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double pi = Math.PI;
+            Console.Write("pi: ");
+            Console.WriteLine(pi);
+            double e = Math.E;
+            Console.Write("e: ");
+            Console.WriteLine(e);
+            Console.ReadKey();
         }
     }
 }
