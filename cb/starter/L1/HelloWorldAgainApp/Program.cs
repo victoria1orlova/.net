3c662c80e6f9c10f4d01612b397061200cd1bc5c
Victoria <victoria1orlova@gmail.com>
2020-06-03 10:52:10 +0300

    I come back

---
+++ cb/starter/L1/HelloWorldAgainApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldAgainApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+            Console.ReadKey();
+        }
+    }
+}
