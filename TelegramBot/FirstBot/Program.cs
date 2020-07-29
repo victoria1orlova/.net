cf3db8c31f88d5989e0e63ed8ee8c9d5a3931dae
Victoria <victoria1orlova@gmail.com>
2020-07-29 15:02:52 +0300

    FirstBot has be started

---
+++ TelegramBot/FirstBot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace FirstBot
+{
+    class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("1379242676:AAElTeOUGJv21teEWtMTBGRanL5cKJzvQuk");
+
+        static void Main(string[] args)
+        {
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.OnMessageEdited += Bot_OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.TextMessage)
+            {
+                if (e.Message.Text == "How are you?")
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Fine, thank you) And you?");
+                else if (e.Message.Text == "Good morning)")
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Good morning, " + e.Message.Chat.Username);
+                }
+                else
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, @"Usage:                            
+                       How are you?                            
+                       Good morning!
+                        ");
+                }
+            }
+        }
+    }
+}
