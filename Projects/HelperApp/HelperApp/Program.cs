5a1e47745c267a7d70c261c8372723a84d9ffe7f
VikaPaw0990 <victoria1orlova@gmail.com>
2020-11-08 02:26:18 -0800

    HelperApp first part is done

--- Projects/HelperApp/HelperApp/Program.cs
+++ Projects/HelperApp/HelperApp/Program.cs
@@ -1,9 +1,36 @@
-﻿namespace HelperApp
+﻿using System;
+
+namespace HelperApp
 {
     class Program
     {
         static void Main()
         {
+            Console.WriteLine("-=Welcome \n     to\n    HelperApp!");
+
+            // 1
+            Console.WriteLine("+");
+            Console.WriteLine("\n");
+
+            // 2.1
+            Console.WriteLine("++++++++++");
+            Console.WriteLine("\n");
+
+            // 2.2
+            for (int i = 0; i < 20; i++)
+            {
+                Console.Write("+");
+            }
+            Console.WriteLine("\n");
+
+            // 3
+            Console.WriteLine("++++\n" +
+                              "++++\n" +
+                              "++++\n" +
+                              "++++\n");
+
+            Console.WriteLine("Exit");
+            Console.ReadKey();
         }
     }
 }
