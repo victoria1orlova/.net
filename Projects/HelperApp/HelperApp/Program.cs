0f1b4b025091f009d4177e3dabdadce0a0739dbc
VikaPaw0990 <victoria1orlova@gmail.com>
2020-11-08 10:20:22 -0800

    HelperApp next part is done

--- Projects/HelperApp/HelperApp/Program.cs
+++ Projects/HelperApp/HelperApp/Program.cs
@@ -6,7 +6,7 @@ namespace HelperApp
     {
         static void Main()
         {
-            Console.WriteLine("-=Welcome \n     to\n    HelperApp!");
+            Console.WriteLine("                        Welcome \n                           to\n                        HelperApp!");
 
             // 1
             Console.WriteLine("+");
@@ -28,7 +28,46 @@ namespace HelperApp
                               "++++\n" +
                               "++++\n" +
                               "++++\n");
+            Console.WriteLine("\n");
 
+            // 4
+            Console.WriteLine("++++++++++\n" +
+                              "++++++++++\n" +
+                              "++++++++++\n" +
+                              "++++++++++\n");
+            Console.WriteLine("\n");
+
+            // 5
+            Console.WriteLine("       +       \n" +
+                              "      +++      \n" +
+                              "     +++++     \n" +
+                              "    +++++++    \n" +
+                              "   +++++++++   \n" +
+                              "  +++++++++++  \n" +
+                              " +++++++++++++ \n");
+            Console.WriteLine("\n");
+            // 6
+            Console.WriteLine("       +       \n" +
+                              "      ***o     \n" +
+                              "    0*****     \n" +
+                              "    *******O   \n" +
+                              "   *****6***   \n" +
+                              " 8***********  \n" +
+                              " ************* \n");
+            Console.WriteLine("\n");
+            // 7
+            Console.WriteLine(" *           *       \n" +
+                              "   *     *       *   \n" +
+                              "        *    *       \n" +
+                              "   *             *   \n" +
+                              "       *     *     * \n" +
+                              "    *      *         \n" +
+                              "      *          *   \n");
+            Console.WriteLine("\n");
+            // 8
+            Console.WriteLine("Autors: Atrtur Murmulyk & Mihail Orlov\n" +
+                              "Writer program: Victoria Orlova _/(O w O)/-\n");
+            Console.WriteLine("\n");
             Console.WriteLine("Exit");
             Console.ReadKey();
         }
