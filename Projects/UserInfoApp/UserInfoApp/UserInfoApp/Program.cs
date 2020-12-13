14086a59df49247f75dbaf176116b1c4224f3762
VikaPaw0990 <victoria1orlova@gmail.com>
2020-12-12 16:52:59 -0800

    Added proj UserInfoApp & did Task1

---
+++ Projects/UserInfoApp/UserInfoApp/UserInfoApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UserInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to UserInfoApp!");
+            Console.WriteLine("Hi!\nWhat's your name?)");
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi {name}!");
+
+            Console.WriteLine("\nWhat's your second name?)");
+            Console.Write("Enter your second name: ");
+            string secondName = Console.ReadLine();
+            Console.WriteLine($"Hi {name},{secondName}!");
+
+            Console.WriteLine("\nHow old are you?)");
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"Your age: {age}!");
+            Console.WriteLine($"\nYour name: {name}\nYour second name: {secondName}\nYour age: {age})");
+            Console.ReadKey();
+        }
+    }
+}
