d869a5c44e4d0c39765850162b4db47012307679
VikaPaw0990 <victoria1orlova@gmail.com>
2021-01-05 12:53:01 -0800

    Added proj MathApp

---
+++ Projects/MathApp/MathApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MathApp!");
+        }
+    }
+}
---
+++ Projects/MathApp/MathApp/ReadMe.txt
@@ -0,0 +1,3 @@
+﻿1.Prompt user for console input of 2 numbers
+2.Ask the user what arithmetic operation he wants to perform
+3.And display the calculations of these numbers (sum, difference, etc.)
