8e4ec6ad489dd66f930888255576e5d5bcaad661
VikaPaw0990 <victoria1orlova@gmail.com>
2021-01-05 16:52:24 -0800

    MathApp task2 is done

--- Projects/MathApp/MathApp/Program.cs
+++ Projects/MathApp/MathApp/Program.cs
@@ -7,6 +7,21 @@ namespace MathApp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to MathApp!");
+            Console.WriteLine("\nWrite any two numbers)");
+            Console.Write("Enter here: ");
+            string numbers = Console.ReadLine();
+            Console.WriteLine($"Your numbers: {numbers}!");
+
+            Console.WriteLine("\nWhat arithmetic operation do you want to perform?)");
+            Console.Write("Write arithmetic operation here: ");
+            string ArithmeticOperation = Console.ReadLine();
+            Console.WriteLine($"{ArithmeticOperation}=4");
+
+            Console.WriteLine($"\n({numbers})-({ArithmeticOperation})=6");
+            Console.Write("Am I right?");
+            Console.ReadLine();
+            Console.WriteLine($"\nOkay");
+            Console.ReadKey();
         }
     }
 }
--- Projects/MathApp/MathApp/ReadMe.txt
+++ Projects/MathApp/MathApp/ReadMe.txt
@@ -1,3 +1,3 @@
-﻿1.Prompt user for console input of 2 numbers
-2.Ask the user what arithmetic operation he wants to perform
-3.And display the calculations of these numbers (sum, difference, etc.)
+﻿+ Prompt user for console input of 2 numbers
++ 2. Ask the user what arithmetic operation he wants to performsx
++ 3. And display the calculations of these numbers (sum, difference, etc.)
