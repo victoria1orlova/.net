4735f93b49244e2e566ea372e3b9318b5d3c8b56
VikaPaw0990 <victoria1orlova@gmail.com>
2021-01-07 10:21:18 -0800

    MathApp changed

--- Projects/MathApp/MathApp/Program.cs
+++ Projects/MathApp/MathApp/Program.cs
@@ -7,21 +7,18 @@ namespace MathApp
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to MathApp!");
-            Console.WriteLine("\nWrite any two numbers)");
-            Console.Write("Enter here: ");
-            string numbers = Console.ReadLine();
-            Console.WriteLine($"Your numbers: {numbers}!");
 
-            Console.WriteLine("\nWhat arithmetic operation do you want to perform?)");
-            Console.Write("Write arithmetic operation here: ");
-            string ArithmeticOperation = Console.ReadLine();
-            Console.WriteLine($"{ArithmeticOperation}=4");
+            Console.Write("Enter here number 'a': ");
+            string numA = Console.ReadLine();
+            int a = int.Parse(numA);
 
-            Console.WriteLine($"\n({numbers})-({ArithmeticOperation})=6");
-            Console.Write("Am I right?");
-            Console.ReadLine();
-            Console.WriteLine($"\nOkay");
-            Console.ReadKey();
+            Console.Write("Enter here number 'b': ");
+            string numB = Console.ReadLine();
+            double b = double.Parse(numB);
+            
+            Console.WriteLine($"a={a}  b={b}");
+
+            Console.WriteLine($"{a}+{b}={a + b}");
         }
     }
 }
