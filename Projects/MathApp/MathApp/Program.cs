14157e895a1170f4150485e5b81196a0d6ca1aa4
VikaPaw0990 <victoria1orlova@gmail.com>
2021-01-10 11:47:05 -0800

    MathApp can looping & exiting

--- Projects/MathApp/MathApp/Program.cs
+++ Projects/MathApp/MathApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MathApp
 {
@@ -8,49 +9,57 @@ namespace MathApp
         {
             Console.WriteLine("Welcome to MathApp!");
 
-            Console.Write("Enter here number 'a': ");
-            string numA = Console.ReadLine();
-            var a = double.Parse(numA);
-
-            Console.Write("Enter here number 'b': ");
-            string numB = Console.ReadLine();
-            var b = double.Parse(numB);
-
-            Console.Write("Enter math operation: ");
-
-            string o = Console.ReadLine();
-            var c = 0.0;
-
-            switch (o)
+            do
             {
-                case "+":
-                    c = a + b;
-                    break;
-                case "-":
-                    c = a - b;
-                    break;
-                case "*":
-                    c = a * b;
-                    break;
-                case "/":
-                    c = a / b;
-                    break;
-            }
+                Console.Write("Enter here number 'a': ");
+                string numA = Console.ReadLine();
+                var a = double.Parse(numA);
 
-            Console.WriteLine($"{a}{o}{b}={c}");
+                Console.Write("Enter here number 'b': ");
+                string numB = Console.ReadLine();
+                var b = double.Parse(numB);
 
-            if (a > b)
-            {
-                Console.WriteLine($"Number {a} biggest than number {b}");
-            }
-            else if(b > a)
-            {
-                Console.WriteLine($"Number {b} biggest than number {a}");
-            }
-            else
-            {
-                Console.WriteLine($"Number {a} equal to number {b}");
-            }
+                Console.Write("Enter math operation: ");
+
+                string o = Console.ReadLine();
+                var c = 0.0;
+
+                switch (o)
+                {
+                    case "+":
+                        c = a + b;
+                        break;
+                    case "-":
+                        c = a - b;
+                        break;
+                    case "*":
+                        c = a * b;
+                        break;
+                    case "/":
+                        c = a / b;
+                        break;
+                }
+
+                Console.WriteLine($"{a}{o}{b}={c}");
+
+                if (a > b)
+                {
+                    Console.WriteLine($"Number {a} biggest than number {b}");
+                }
+                else if (b > a)
+                {
+                    Console.WriteLine($"Number {b} biggest than number {a}");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {a} equal to number {b}");
+                }
+
+                Thread.Sleep(3000);
+                Console.Clear();
+                Console.WriteLine("For EXIT press 'Q'");
+
+            } while (Console.ReadKey().Key != ConsoleKey.Q);
         }
     }
 }
