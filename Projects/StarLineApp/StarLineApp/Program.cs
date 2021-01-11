0a30b98ccc3200f7d05384e0ef638852659fb69d
VikaPaw0990 <victoria1orlova@gmail.com>
2021-01-11 11:08:28 -0800

    StarLineApp give '*' so many stars the user wrote so many

---
+++ Projects/StarLineApp/StarLineApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StarLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-= STAR LINE =-");
+            Console.Write("Write the number of asterisks '*' --> ");
+            var userInput = Console.ReadLine();
+
+            var lenght = int.Parse(userInput);
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
