7599720b25b1bb8f517f5e5029fbdcf189275ebf
VikaPaw0990 <victoria1orlova@gmail.com>
2021-02-05 05:59:03 -0800

    AntiFloodTelegramBot can display name and message from user

---
+++ Projects/AntiFloodTelegramBot/AntiFloodTelegramBot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Telegram.Bot;
+
+namespace AntiFloodTelegramBot
+
+{
+    class Program
+    {
+        static TelegramBotClient Bot;
+
+        static void Main(string[] args)
+        {
+            Bot = new TelegramBotClient("1670447707:AAHshrI-pis_i6cXIRkxsjF9I4hBzOanDRw");
+
+            Bot.OnMessage += Bot_OnMessage;
+
+            var me = Bot.GetMeAsync().Result;
+
+            Console.WriteLine(me.FirstName);
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            var message = e.Message;
+            
+            string name = $"{message.From.FirstName} {message.From.LastName}";
+
+            Console.WriteLine($" {name} sended message: '{message.Text}'");
+        }
+    }
+}
