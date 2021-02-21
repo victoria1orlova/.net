30156fc4fcb05229a1ff513ba47009676f2718c8
VikaPaw0990 <victoria1orlova@gmail.com>
2021-02-21 03:07:04 -0800

    OperationApp can sum, subtract, multiply, divide now only two numbers

---
+++ tests/OperationApp/OperationApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace OperationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display();
+        }
+
+        static int Sum()
+        {
+            var n1 = 5;
+            var n2 = 6;
+            //Console.WriteLine($"{n1}+{n2}");
+            return n1 + n2;
+        }
+
+        static int Sub(int n1, int n2)
+        {
+            //Console.WriteLine($"{n1}-{n2}");
+            return n1 - n2;
+        }
+
+        static int Mul()
+        {
+            var n1 = 5;
+            var n2 = 6;
+            //Console.WriteLine($"{n1}*{n2}");
+            return n1 * n2;
+        }
+
+        static int Div(int n1, int n2)
+        {
+            //Console.WriteLine($"{n1}/{n2}");
+            return n1 / n2;
+        }
+
+        static void Display()
+        {
+            Console.WriteLine("a + b = {0}", Sum());
+            Console.WriteLine("a - b = {0}", Sub(2, 3));
+            Console.WriteLine("a * b = {0}", Mul());
+            Console.WriteLine("a / b = {0}", Div(4, 7));
+        }
+
+    }
+}
