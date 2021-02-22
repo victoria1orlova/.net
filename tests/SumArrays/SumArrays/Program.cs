f897e046630c62385c6b5a1d5597180f8ec1cca4
VikaPaw0990 <victoria1orlova@gmail.com>
2021-02-22 07:10:35 -0800

    SumArrays can calculate inscribed numbers

---
+++ tests/SumArrays/SumArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new[] { 2, 7, 5, 2, 12 };
+
+            int sum = 0;
+            int sub = 0;
+
+            foreach (var i in array)
+            {
+                sum = sum + i;
+            }
+
+            sub = array[0] - array[1] - array[2] - array[3] - array[4];
+
+            Console.WriteLine(sum);
+            Console.WriteLine(sub);
+        }
+    }
+}
