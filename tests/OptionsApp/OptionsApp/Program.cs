af5699b0bc24b31333a2173ae231ac368aa26df6
VikaPaw0990 <victoria1orlova@gmail.com>
2021-02-26 08:04:10 -0800

    Options with out can find area & perimetr

--- tests/OptionsApp/OptionsApp/Program.cs
+++ tests/OptionsApp/OptionsApp/Program.cs
@@ -6,7 +6,18 @@ namespace OptionsApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int area;
+            int perimetr;
+            GetData(3, 4, out area, out perimetr);
+            Console.WriteLine("Area : " + area);
+            Console.WriteLine("Perimetr : " + perimetr);
+
+            Console.ReadKey();
+        }
+        static void GetData(int a, int b, out int area, out int perim)
+        {
+            area = a * b;
+            perim = (a + b) * 2;
         }
     }
 }
