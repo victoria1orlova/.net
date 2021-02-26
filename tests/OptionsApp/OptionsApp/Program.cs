56ea8017ce141450a27ed9ce74c63410e88b0912
VikaPaw0990 <victoria1orlova@gmail.com>
2021-02-26 07:55:22 -0800

    Options is empty

---
+++ tests/OptionsApp/OptionsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OptionsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
