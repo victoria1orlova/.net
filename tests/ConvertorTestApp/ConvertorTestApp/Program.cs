b6f2663a90d633a5d9dc1c0585a9b0ac396d427f
VikaPaw0990 <victoria1orlova@gmail.com>
2021-02-27 05:31:11 -0800

    ConvertorTestApp can convers course

---
+++ tests/ConvertorTestApp/ConvertorTestApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConvertorTestApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter course hrn OR dol:");
+            string course = Console.ReadLine();
+
+            Console.Write("Enter NUMBER of dollar course: ");
+
+            int money = Int32.Parse(Console.ReadLine());
+            int result = 0;
+
+            if (course == "hrn")
+            {
+                ConvertHrn(money, ref result);
+            }
+            else
+            {
+                ConvertDol(money, ref result);
+            }
+
+            Console.WriteLine(result);
+        }
+
+        static void ConvertHrn(int money, ref int result)
+        {
+            result = money / 28;
+        }
+        static void ConvertDol(int money, ref int result)
+        {
+            result = money * 28;
+        }
+    }
+}
