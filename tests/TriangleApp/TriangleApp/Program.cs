d587d02d9e002c8c95614f9c44ba314e8352bc3e
VikaPaw0990 <victoria1orlova@gmail.com>
2021-03-22 11:53:15 -0700

    This app can do square

---
+++ tests/TriangleApp/TriangleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TriangleApp
+{
+    class Program
+    {
+        static void Main(string[] args)//2 and 3
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (var u = 0; u < 2; u++)
+                {
+                    Console.Write("+");
+                }
+                Console.Write("\n");//Console.WriteLine(" ");
+
+            }
+        }
+    }
+}
