987f121943094460bd39250c25757b9d0328fe3e
VikaPaw0990 <victoria1orlova@gmail.com>
2021-03-25 11:42:54 -0700

    This app can find arithmetic mean

---
+++ tests/OperationsWithVariablesApp/OperationsWithVariablesApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace OperationsWithVariablesApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first variable -> ");
+            int firstV = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second variable -> ");
+            int secondV = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter third variable -> ");
+            int thirdV = Int32.Parse(Console.ReadLine());
+
+            var operations = new ArithmeticOperations();
+            operations.Average(firstV, secondV, thirdV);
+
+            Console.WriteLine(operations.Sum(firstV, secondV, thirdV));
+            Console.WriteLine(operations.Average(firstV, secondV, thirdV));
+
+            var tell = new Tell();
+            Console.WriteLine("What is your name?");
+
+            var name = Console.ReadLine();
+
+            tell.Greeting(name);
+        }
+        class ArithmeticOperations
+        {
+            public int Sum(int firstV, int secondV, int thirdV)
+            {
+                return firstV + secondV + thirdV;
+            }
+            public int Average(int firstV, int secondV, int thirdV)
+            {
+                return Sum(firstV, secondV, thirdV) / 3;
+            }
+        }
+
+        class Tell
+        {
+            public void Greeting(string yourName)
+            {
+                Console.WriteLine($"Hi,{yourName}:>");
+            }
+        }
+    }
+}
