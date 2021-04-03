6f2e69d11786847a350db20281017ece5be5b91e
VikaPaw0990 <victoria1orlova@gmail.com>
2021-04-03 00:00:55 -0700

    IfExampleApp check user's age

---
+++ tests/IfExampleApp/IfExampleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace IfExampleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello! How old are you?");
+            int age = Int32.Parse(Console.ReadLine());
+
+            AgeCheck(age);
+        }
+
+        static void AgeCheck(int age)
+        {
+            if (age <= 10)
+            {
+                Console.WriteLine("You are too young for this ride!");
+            }
+            else
+            {
+                Console.WriteLine("Welcome to trampolines!)");
+            }
+        }
+    }
+}
