ca56661c4d932b15236416b57ac1e6713af62876
VikaPaw0990 <victoria1orlova@gmail.com>
2021-05-22 20:15:19 -0700

    All changes

---
+++ Projects/ConsoleAnimationApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAnimationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I LOVE YOU)))");
+        }
+    }
+}
---
+++ Projects/StringApp/StringApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            addRecord("124", "");
+        }
+    }
+}
