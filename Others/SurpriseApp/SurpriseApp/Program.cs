cc79927795ff6f100fd04451dba9d2e1b2b32ff5
Victoria Orlova <victoria1orlova@gmail.com>
2021-08-17 19:25:40 -0700

    SurpriseApp started!

---
+++ Others/SurpriseApp/SurpriseApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SurpriseApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Happy birthday!");
+        }
+    }
+}
