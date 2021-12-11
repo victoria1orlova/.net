f6e52ac54e2844355ddfbfadeac086fa1f69e4eb
Victoria Orlova <victoria1orlova@gmail.com>
2021-12-11 20:44:14 +0200

    Added MultiplicationTableApp

---
+++ Projects/MultiplicationTableApp/MultiplicationTableApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiplicationTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 2; a < 10; a++)
+            {
+                for (int i = 2; i < 10; i++)
+                {
+                    Console.WriteLine($"{a} * {i} = {a * i}");
+                }
+            }
+        }
+    }
+}
