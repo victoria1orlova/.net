018480c92548380a04e15dc055cc764e480b560c
Victoria Orlova <victoria1orlova@gmail.com>
2021-12-12 14:15:57 +0200

    Added border table

--- Projects/MultiplicationTableApp/MultiplicationTableApp/Program.cs
+++ Projects/MultiplicationTableApp/MultiplicationTableApp/Program.cs
@@ -6,6 +6,49 @@ namespace MultiplicationTableApp
     {
         static void Main(string[] args)
         {
+            //╔
+            Console.Write($"{(char)9556}");
+            //═
+            for (int l = 0; l < 44; l++)
+            {
+                Console.Write($"{(char)9552}");
+            }
+
+            // ╗
+            Console.Write($"{(char)9559}");
+            Console.Write("\n");
+
+            for (int i = 0; i < 15; i++)
+            {
+                // ║
+                
+            }
+            
+            for (int i = 0; i < 15; i++)
+            {
+                Console.Write($"{(char)9553}");
+
+                for (int d = 0; d < 44; d++)
+                {
+                    Console.Write(" ");
+                }
+                // ║
+                Console.WriteLine($"{(char)9553}");
+            }
+
+            // ╚
+            Console.Write($"{(char)9562}");
+            //═
+            for (int l = 0; l < 44; l++)
+            {
+                Console.Write($"{(char)9552}");
+            }
+
+            // ╝
+            Console.Write($"{(char)9565}");
+
+            return;
+
             for (int a = 2; a < 10; a++)
             {
                 for (int i = 2; i < 10; i++)
