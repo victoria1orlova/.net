6fbb96240c2d5cdd8ace9b5cf10a74b887556f76
Victoria Orlova <victoria1orlova@gmail.com>
2021-12-19 20:06:04 +0200

    ArrayApp is progressing (;

--- Projects/ArrayApp/ArrayApp/Program.cs
+++ Projects/ArrayApp/ArrayApp/Program.cs
@@ -6,7 +6,21 @@ namespace ArrayApp
     {
         static void Main()
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter number: ");
+            string numA = Console.ReadLine();
 
+            int a = int.Parse(numA);
+
+            Console.WriteLine("We got just one number from you. Type here second numb: ");
+            string numB = Console.ReadLine();
+
+            int b = int.Parse(numB);
+
+            var exp = $"{a} * {b} = {a * b}";
+            var value = $"We multiplied it: \n{exp}";
+            Console.WriteLine(value);
         }
+
     }
 }
