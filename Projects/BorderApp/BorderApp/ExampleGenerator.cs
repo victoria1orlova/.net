eba852141c3619092111aac814ef6fa565be88a2
Victoria Orlova <victoria1orlova@gmail.com>
2021-12-28 16:19:37 +0200

    BorderApp added 'if, else'

--- Projects/BorderApp/BorderApp/ExampleGenerator.cs
+++ Projects/BorderApp/BorderApp/ExampleGenerator.cs
@@ -4,13 +4,23 @@
     {
         public string[] Generate()
         {
-            string[] result = new string[3];
+            string[] result = new string[8];
             for (int v = 2; v < 3; v++)
             {
                 for (int i = 2, index = 0; i < result.Length + 2; i++, index++)
                 {
-                    var obj = $"{v} * {i} = {v * i}";
-                    result[index] = obj;
+                    if (i < 5)
+                    {
+                        var space = " ";
+                        var obj = $"{v} * {i} = {v * i}{space}";
+                        result[index] = obj;
+                    }
+                    else
+                    {
+                        var space = " ";
+                        var obj = $"{v} * {i} = {v * i}";
+                        result[index] = obj;
+                    }
                     //Console.WriteLine(obj);
                 }
             }
